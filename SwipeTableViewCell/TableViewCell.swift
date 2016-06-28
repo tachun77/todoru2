5982de3c1f217cb8c4163f612fc15818428ecbdb
Tatsuya <t.mugen282@gmail.com>
2016-06-29 01:57:00 +0900

    cell のデザイン

---
+++ SwipeTableViewCell/TableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TableViewCell.swift
+//  Todoル
+//
+//  Created by 福島達也 on 2016/06/29.
+//  Copyright © 2016年 福島達也. All rights reserved.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
